31461911e671b06987ff4c533013f577313de761
MazenHisham <100722135+MazenHisham@users.noreply.github.com>
2022-03-02 01:05:52 +0200

    Update TASK #1.sql

--- TASK #1.sql
+++ TASK #1.sql
@@ -1,3 +1,4 @@
+------------------Examples------------------
 -- Q1
 select customerName,customerNumber,creditlimit
 From customers
@@ -26,34 +27,38 @@ select orderDate
 From orders
 where orderDate >= '2003-01-01' and '2013-01-01';
 -- Q9
+select country,city
+From customers
+order by country,city;
+-- Q12
+select customerNumber
+From customers
+where customerNumber between 100 and 400;
+-----------------------TASK--------------------------
+-- Q1
 select creditLimit,customerNumber,country
 From customers
-where creditLimit >= 1000000 AND customerNumber <20 OR country ='USA'
--- Q10
+where creditLimit >= 1000000 AND customerNumber <200 OR country ='USA'
+-- Q2
 select creditLimit,creditlimit+200
 From customers
 order by creditlimit+200 DESC;
--- Q11
+-- Q3
 select customerName,creditlimit
 From customers
 order by creditlimit DESC
 limit 3
--- Q12
+-- Q4
 select customerName
 From customers
-where customerName like'ei%'
--- Q13
+where customerName like'le%'
+-- Q5
 select customerName
 From customers
-where customerName like'a%'
--- Q14
-select customerNumber
+where customerName like'le$%' OR 'ie$'
+-- Q6
+select customerName
 From customers
-where customerNumber between 100 and 400;
--- Q15
-select country,city
-From customers
-order by country,city;
-
+where customerName like'g||a%'
 
 
